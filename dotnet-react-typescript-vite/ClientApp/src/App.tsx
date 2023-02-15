2927132e1fe47b77b6eaf2d50fd3645e044107d6
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-02-15 23:36:39 +0200

    Add dotnet and react spa template

---
+++ dotnet-react-typescript-vite/ClientApp/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Start your cool project from this point</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ dotnet-react-typescript-vite/Constants/SwaggerGroup.cs
@@ -0,0 +1,6 @@
+﻿namespace dotnet_react_typescript_vite.Constants;
+
+public static class SwaggerGroup
+{
+    public const string Weather = "Weather";
+}
