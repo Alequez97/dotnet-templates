2c6d855a6a2f4fed29281aaef5fff9840dc58d76
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-02-15 23:59:09 +0200

    Add systemd setup script

---
+++ telegram-bot/Systemd/telegram_bot.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+sudo systemctl stop telegram_bot
+
+git pull
+rm -rf /srv/VirtualCards.TelegramBot
+mkdir /srv/VirtualCards.TelegramBot
+chown root /srv/VirtualCards.TelegramBot
+cd ..
+dotnet publish -c Release -o /srv/VirtualCards.TelegramBot
+
+sudo cp telegram_bot.service /etc/systemd/system/telegram_bot.service
+sudo systemctl daemon-reload
+echo "run sudo systemctl start telegram_bot to start service"
