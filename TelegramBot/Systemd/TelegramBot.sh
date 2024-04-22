acf66b3369f099ca5f897222709ba7dab95b147c
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-04-22 17:50:22 +0300

    fix: directory in systemd service publish script

--- TelegramBot/Systemd/TelegramBot.sh
+++ TelegramBot/Systemd/TelegramBot.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/bash
 
-sudo systemctl stop TelegramBotTemplate
+systemctl stop TelegramBotTemplate
 
 git pull
 rm -rf /srv/TelegramBotTemplate
@@ -8,7 +8,7 @@ mkdir /srv/TelegramBotTemplate
 chown root /srv/TelegramBotTemplate
 cd ..
 dotnet publish -c Release -o /srv/TelegramBotTemplate
-
-sudo cp TelegramBotTemplate.service /etc/systemd/system/TelegramBotTemplate.service
-sudo systemctl daemon-reload
+cd ./Systemd
+cp TelegramBotTemplate.service /etc/systemd/system/TelegramBotTemplate.service
+systemctl daemon-reload
 echo "run sudo systemctl start TelegramBotTemplate to start service"
